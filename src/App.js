adeea31f98a476d861a76d564dd5aec1fc2c8f38
Kate Lockhart <63356074+KateLockhart@users.noreply.github.com>
2022-08-25 20:00:59 -0400

    Added basic MD component (Post)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Post } from "./Post";
 import "./App.css";
 
+const posts = {
+  post1: "./posts/test.md",
+  post2: "./posts/test2.md",
+  post3: "./posts/test3.md",
+};
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {Object.values(posts).map((url) => {
+        return <Post key={url} postUrl={url} />;
+      })}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Post.js
@@ -0,0 +1,30 @@
+import Markdown from "markdown-to-jsx";
+import { useEffect, useState } from "react";
+import PostNotFound from "./errors/postNotFound.md";
+
+export const Post = ({ postUrl }) => {
+  const url = `${process.env.PUBLIC_URL}${postUrl}`;
+  const [content, setContent] = useState("");
+
+  //console.log(postUrl);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        const contentType = res.headers.get("content-type");
+        if (contentType && contentType.indexOf("text/markdown") !== -1)
+          return res.text();
+
+        return notFoundHandler();
+      })
+      .then((text) => setContent(text));
+  }, [url]);
+
+  return <Markdown>{content}</Markdown>;
+};
+
+async function notFoundHandler() {
+  let result = fetch(PostNotFound);
+  let text = await (await result).text();
+  return text;
+}
